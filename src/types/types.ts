afca3922f8ab20b37deae6ca00f8a4bf3bfc7f7a
Min Phone Aung <88699874+PeterHyperAung@users.noreply.github.com>
2023-07-06 01:32:31 +0630

    organizations page partially completed

---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export interface OrganizationType {
+  name: string;
+  slug: string;
+  url: string;
+  description: string;
+  field: string | null;
+  goal: string | null;
+  base: string | null;
+}
